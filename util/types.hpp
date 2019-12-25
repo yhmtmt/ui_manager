674e05fe693abcf3192c7f77de5c16cf3ee9aefd
yhmtmt <yhmtmt@gmail.com>
2019-12-25 14:30:49 +0900

    Missing file

---
+++ util/types.hpp
@@ -0,0 +1,11 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+enum e_ui_mode {
+  ui_mode_fpv, ui_mode_map, ui_mode_sys, ui_mode_undef
+};
+
+enum e_ui_obj{
+  ui_obj_wp = 0, ui_obj_vsl, ui_obj_mrk, ui_obj_cl, ui_obj_undef 
+};
+
+#endif
